c43b388127e78522d9ef9ab5e8d5a88b1fde82e8
eschlatter <eschlatter1@gmail.com>
2024-07-05 00:49:36 -0400

    Messing around with processing the tree sequence in Python

--- analysis/3_AddMutations.py
+++ analysis/3_AddMutations.py
@@ -1,5 +1,19 @@
-import msprime, tskit
+import msprime, tskit, pyslim
 
-ts = tskit.load("../output/final.trees").simplify()
-mutated = msprime.mutate(ts, rate=1e-7, random_seed=1, keep=True)
-mutated.dump("../output/final_overlaid.trees")
+ts = tskit.load("../output/slim_2003563054.trees")
+
+# individuals have a "location" property:
+ind = ts.individual(0)
+ind
+
+
+
+#ts = ts.simplify()
+
+# check for coalescence
+for t in ts.trees():
+    assert t.num_roots == 1, ("not coalesced! on segment {} to {}".
+        format(t.interval[0], t.interval[1]))
+
+#mutated = msprime.mutate(ts, rate=1e-7, random_seed=1, keep=True)
+#mutated.dump("../output/final_overlaid.trees")
